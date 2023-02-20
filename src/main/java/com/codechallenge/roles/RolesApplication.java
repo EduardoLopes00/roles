3eb93da37100a6c1c9b3372a78ad07d0ad66ec8d
Eduardo Lopes <eduardo.toworkstudy@gmail.com>
2023-02-20 04:09:54 -0300

    feat: rename package

--- src/main/java/com/codechallenge/roles/RolesApplication.java
+++ src/main/java/com/codechallenge/roles/RolesApplication.java
@@ -1,4 +1,4 @@
-package com.codetest.roles;
+package com.codechallenge.roles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
