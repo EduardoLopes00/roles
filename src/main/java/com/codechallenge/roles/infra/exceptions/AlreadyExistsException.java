c91a040e85ef613f6059c86766000249e857c648
Eduardo Lopes <eduardo.toworkstudy@gmail.com>
2023-02-21 03:57:46 -0300

    refactor: Removing unused constructor from personalized exceptions

--- src/main/java/com/codechallenge/roles/infra/exceptions/AlreadyExistsException.java
+++ src/main/java/com/codechallenge/roles/infra/exceptions/AlreadyExistsException.java
@@ -1,11 +1,8 @@
 package com.codechallenge.roles.infra.exceptions;
 
 public class AlreadyExistsException extends RuntimeException{
-    public<T> AlreadyExistsException(T searchData){
+    public AlreadyExistsException (String searchData){
         super("The system couldn't complete the action because the item already exists for data " + searchData);
     }
 
-    public AlreadyExistsException(String message) {
-        super(message);
-    }
 }
--- src/main/java/com/codechallenge/roles/infra/exceptions/NotFoundException.java
+++ src/main/java/com/codechallenge/roles/infra/exceptions/NotFoundException.java
@@ -1,11 +1,9 @@
 package com.codechallenge.roles.infra.exceptions;
 
 public class NotFoundException extends RuntimeException{
-    public<T> NotFoundException(T searchData) {
+    public NotFoundException(String searchData) {
         super("Item not found for ID " + searchData);
     }
 
-    public NotFoundException(String message) {
-        super(message);
-    }
+
 }
