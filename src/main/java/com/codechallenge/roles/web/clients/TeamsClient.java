4ca51b7b25e8400aa85ab49f9e165bb00246ea9c
Eduardo Lopes <eduardo.toworkstudy@gmail.com>
2023-02-21 01:26:38 -0300

    feat: Create Teams client interface

---
+++ src/main/java/com/codechallenge/roles/web/clients/TeamsClient.java
@@ -0,0 +1,34 @@
+package com.codechallenge.roles.web.clients;
+
+
+import com.codechallenge.roles.data.models.Team;
+import com.codechallenge.roles.web.DTO.TeamsDTO;
+import com.codechallenge.roles.web.interfaces.TeamsClientInterface;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import java.util.List;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Component
+public class TeamsClient implements TeamsClientInterface {
+
+    @Autowired
+    private WebClient teamsWebClient;
+
+    public List<TeamsDTO> getTeams() {
+        List<TeamsDTO> teams = this.teamsWebClient.method(HttpMethod.GET).retrieve().bodyToMono(new ParameterizedTypeReference<List<TeamsDTO>>() {}).block();
+
+        return teams;
+    }
+
+    public Team getTeamById(UUID teamId) {
+        Team team = this.teamsWebClient.method(HttpMethod.GET).uri("/" + teamId).retrieve().bodyToMono(Team.class).block();
+
+        return team;
+    }
+}
---
+++ src/main/java/com/codechallenge/roles/web/interfaces/TeamsClientInterface.java
@@ -0,0 +1,13 @@
+package com.codechallenge.roles.web.interfaces;
+
+import com.codechallenge.roles.data.models.Team;
+import com.codechallenge.roles.web.DTO.TeamsDTO;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface TeamsClientInterface {
+    public List<TeamsDTO> getTeams();
+
+    public Team getTeamById(UUID teamId);
+}
